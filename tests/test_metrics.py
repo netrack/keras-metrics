9667219542bacd11e177bcaca18f2401a4b4517a
Yasha Bubnov <ybubnov@gojuno.com>
2018-11-16 17:00:07 +0300

    Make metrics stateful
    This patch makes metrics stateful.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,4 +1,5 @@
 import keras
+import keras.backend
 import keras_metrics
 import numpy
 import unittest
@@ -8,6 +9,7 @@ class TestMetrics(unittest.TestCase):
 
     def test_metrics(self):
         tp = keras_metrics.true_positive()
+        tn = keras_metrics.true_negative()
         fp = keras_metrics.false_positive()
         fn = keras_metrics.false_negative()
 
@@ -16,27 +18,31 @@ class TestMetrics(unittest.TestCase):
         f1 = keras_metrics.f1_score()
 
         model = keras.models.Sequential()
-        model.add(keras.layers.Dense(1, activation="sigmoid", input_dim=2))
-        model.add(keras.layers.Dense(1, activation="softmax"))
+        model.add(keras.layers.Activation(keras.backend.sin))
+        model.add(keras.layers.Activation(keras.backend.abs))
 
         model.compile(optimizer="sgd",
                       loss="binary_crossentropy",
-                      metrics=[tp, fp, fn, precision, recall, f1])
+                      metrics=[tp, tn, fp, fn, precision, recall, f1])
 
-        samples = 1000
-        x = numpy.random.random((samples, 2))
+        samples = 10000
+        batch_size = 100
+        lim = numpy.pi/2
+
+        x = numpy.random.uniform(0, lim, (samples, 1))
         y = numpy.random.randint(2, size=(samples, 1))
 
-        model.fit(x, y, epochs=1, batch_size=10)
-        metrics = model.evaluate(x, y, batch_size=10)[1:]
+        model.fit(x, y, epochs=10, batch_size=batch_size)
+        metrics = model.evaluate(x, y, batch_size=batch_size)[1:]
 
         tp_val = metrics[0]
-        fp_val = metrics[1]
-        fn_val = metrics[2]
+        tn_val = metrics[1]
+        fp_val = metrics[2]
+        fn_val = metrics[3]
 
-        precision = metrics[3]
-        recall = metrics[4]
-        f1 = metrics[5]
+        precision = metrics[4]
+        recall = metrics[5]
+        f1 = metrics[6]
 
         expected_precision = tp_val / (tp_val + fp_val)
         expected_recall = tp_val / (tp_val + fn_val)
@@ -45,9 +51,16 @@ class TestMetrics(unittest.TestCase):
         f1_divisor = (expected_precision+expected_recall)
         expected_f1 = (2 * f1_divident / f1_divisor)
 
-        self.assertAlmostEqual(expected_precision, precision, delta=0.05)
-        self.assertAlmostEqual(expected_recall, recall, delta=0.05)
-        self.assertAlmostEqual(expected_f1, f1, delta=0.05)
+        self.assertGreaterEqual(tp_val, 0.0)
+        self.assertGreaterEqual(fp_val, 0.0)
+        self.assertGreaterEqual(fn_val, 0.0)
+        self.assertGreaterEqual(tn_val, 0.0)
+
+        self.assertEqual(sum(metrics[0:4]), samples)
+
+        self.assertAlmostEqual(expected_precision, precision)
+        self.assertAlmostEqual(expected_recall, recall)
+        self.assertAlmostEqual(expected_f1, f1)
 
 
 if __name__ == "__main__":
